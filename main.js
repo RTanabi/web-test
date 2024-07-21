377dc1ece642d299c39ccab78915e399d982b97d
RTanabi <rezatanabijob@gmail.com>
2024-07-21 22:11:13 +0330

    update

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 var swiper = new Swiper(".rigth", {
   spaceBetween: 0,
   centeredSlides: true,
+  loop: true,
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
 var swiper = new Swiper(".first", {
   spaceBetween: 0,
   centeredSlides: true,
+  loop: true,
   autoplay: {
-    delay: 1000,
+    delay: 3000,
     disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
