0b4a194dbecaf11f4da17f2a9338a9187c315b37
RTanabi <rezatanabijob@gmail.com>
2024-07-21 21:44:01 +0330

    first

---
+++ main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".rigth", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".first", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+});
